a5fcd41539c49c4c42a05b01ebe63ef9dc16ace7
shadymeowy <tolgademirdal@gmail.com>
2023-02-19 14:45:34 +0300

    packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python-bing-stiles',
+    version='0.0.1',
+    description='Small library for accessing Bing Static Maps API',
+    author='Tolga Demirdal',
+    url='https://github.com/shadymeowy/python-bing-stiles',
+    setup_requires=[],
+    install_requires=['requests'],
+    packages=['bingstiles'],
+    entry_points={
+    },
+    extras_require={
+    },
+)
+ 
