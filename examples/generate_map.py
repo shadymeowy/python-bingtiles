b86f204829eda4a96cbef6797729177bafb5dbb4
shadymeowy <tolgademirdal@gmail.com>
2023-03-25 06:07:41 +0300

    update map generation example

--- examples/generate_map.py
+++ examples/generate_map.py
@@ -4,15 +4,24 @@ if __name__ == '__main__':
     # Specify corner coordinates of the map
     geo1 = 39.89250693, 32.78414742
     geo2 = 39.89055783, 32.7869366
+    # Specify the level of detail
+    lod = 19
     # Create cached fetcher if you want to cache the tiles
-    # fetcher = CachedFetcher('tiles.zip')
+    # fetcher = CachedFetcher('tiles')
     # Otherwise, use the default fetcher
     fetcher = None
     # Parameters for the map
     provider = providers['bing_aerial']
-    params = dict(provider=provider, lod=18)
+    params = dict(
+        provider=provider,
+        fetcher=fetcher,
+        progress=True,
+        parallel=True,
+    )
     # Generate the map
-    img = generate_map(geo1, geo2, progress=True, fetcher=fetcher, **params)
+    img = generate_map(geo1, geo2, lod, **params)
+    # Close the fetcher if it is a cached fetcher
+    # fetcher.close()
     # Show the map
     img.show()
     # Save the map
