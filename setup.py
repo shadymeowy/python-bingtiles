c8f1423767c9a95f44de25ff71128e053635d86d
shadymeowy <tolgademirdal@gmail.com>
2023-04-01 07:30:00 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,13 @@ setup(
     url='https://github.com/shadymeowy/python-bing-tiles',
     setup_requires=[],
     install_requires=['requests', 'Pillow'],
-    packages=['bingtiles'],
+    packages=['bingtiles', 'bingtiles.provider'],
     entry_points={
         'console_scripts': [
-            'bingtiles = bingtiles.__main__:main',              
-        ],          
+            'bingtiles = bingtiles.__main__:main',
+        ],
     },
     extras_require={
         'processbar': ['tqdm'],
     },
 )
- 
