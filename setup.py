3c92e6c784f7e5afd01a1588a9da11f8abc51190
shadymeowy <tolgademirdal@gmail.com>
2023-04-01 07:39:38 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='python-bing-tiles',
+    name='bingtiles',
     version='0.0.1',
     description='Small library for accessing Bing Static Maps API',
     author='Tolga Demirdal',
     url='https://github.com/shadymeowy/python-bing-tiles',
     setup_requires=[],
     install_requires=['requests', 'Pillow'],
-    packages=['bingtiles', 'bingtiles.provider'],
+    packages=['bingtiles'],
     entry_points={
         'console_scripts': [
-            'bingtiles = bingtiles.__main__:main',
-        ],
+            'bingtiles = bingtiles.__main__:main',              
+        ],          
     },
     extras_require={
         'processbar': ['tqdm'],
     },
 )
+ 
