8505e0f7e30a64cd2c68f5007b0084628b041dd4
shadymeowy <tolgademirdal@gmail.com>
2023-08-12 16:50:41 +0300

    sane cached map generation

--- bingtiles/mapgen.py
+++ bingtiles/mapgen.py
@@ -1,9 +1,8 @@
-import math
 import numpy as np
 from PIL import Image
 from multiprocessing.dummy import Pool as ThreadPool
-from functools import lru_cache
 from tqdm import tqdm
+from functools import lru_cache
 
 try:
     import cv2
@@ -12,6 +11,92 @@ except ImportError:
 
 from .utils import geodetic2tile
 from .fetch import fetch_tile
+
+
+class MapGenerator:
+    def __init__(self, provider=None, fetcher=None, progress=False, parallel=True):
+        self.provider = provider
+        self.fetcher = fetcher
+        self.progress = progress
+        self.parallel = parallel
+
+        if self.fetcher is None:
+            self.fetcher = fetch_tile
+        if self.parallel:
+            self.pool = ThreadPool()
+
+        self._rough_gen = lru_cache(maxsize=4)(self._rough_gen)
+
+    def generate_map(self, geo1, geo2, lod, as_array=False):
+        compound = calculate_coverage(geo1, geo2, lod)
+        tile_mn, tile_mx, tile_mn_frac, tile_mx_frac = compound
+
+        tile_mn = tuple(map(int, tile_mn))
+        tile_mx = tuple(map(int, tile_mx))
+        image = self._rough_gen(tile_mn, tile_mx, lod)
+        tile_mx_frac -= 256
+        e0 = tile_mx_frac[0] if tile_mx_frac[0] != 0 else None
+        e1 = tile_mx_frac[1] if tile_mx_frac[1] != 0 else None
+        image_cropped = image[tile_mn_frac[1]:e1, tile_mn_frac[0]:e0]
+        if not as_array:
+            image_cropped = Image.fromarray(image_cropped)
+        return image_cropped
+
+    __call__ = generate_map
+
+    def _rough_gen(self, tile_mn, tile_mx, lod):
+        tile_mn = np.array(tile_mn, np.int32)
+        tile_mx = np.array(tile_mx, np.int32)
+        tile_size = tile_mx - tile_mn
+        poses = _tile_grid(tile_mn, tile_mx, lod)
+        if self.parallel:
+            tiles = self.pool.imap(self._fetch, poses)
+            if self.progress:
+                tiles = list(tqdm(tiles, total=len(poses)))
+            else:
+                tiles = list(tiles)
+        else:
+            if self.progress:
+                poses = tqdm(poses)
+            tiles = list(map(self._fetch, poses))
+        images = [list() for _ in range(tile_size[0] + 1)]
+
+        for tile in tiles:
+            x, image = tile
+            images[x - poses[0][0]].append(image)
+
+        images = [np.concatenate(row, axis=0) for row in images]
+        image = np.concatenate(images, axis=1)
+        return image
+
+    def _fetch(self, pos):
+        if self.provider is None:
+            return (pos[0], self.fetcher(tuple(pos.tolist()), as_array=True))
+        else:
+            return (pos[0], self.fetcher(tuple(pos.tolist()), provider=self.provider, as_array=True))
+
+    def close(self):
+        if self.parallel:
+            self.pool.close()
+            self.pool.join()
+
+    def __del__(self):
+        self.close()
+
+
+def generate_map(geo1, geo2, lod=18, provider=None, progress=False, parallel=True, as_array=False, fetcher=None):
+    generator = MapGenerator(
+        provider=provider, fetcher=fetcher, progress=progress, parallel=parallel)
+    return generator.generate_map(geo1, geo2, lod, as_array=as_array)
+
+
+def _tile_grid(tile_mn, tile_mx, lod):
+    xs = np.arange(tile_mn[0], tile_mx[0] + 1, dtype=np.int32)
+    ys = np.arange(tile_mn[1], tile_mx[1] + 1, dtype=np.int32)
+    poses = np.array(np.meshgrid(xs, ys)).T.reshape(-1, 2)
+    zs = lod * np.ones(len(poses), dtype=np.int32)
+    poses = np.concatenate([poses, zs[:, None]], axis=1)
+    return poses
 
 
 def calculate_coverage(geo1, geo2, lod=18):
@@ -28,68 +113,6 @@ def calculate_coverage(geo1, geo2, lod=18):
     return tile_mn, tile_mx, tile_mn_frac, tile_mx_frac
 
 
-def _tile_grid(tile_mn, tile_mx, lod):
-    xs = np.arange(tile_mn[0], tile_mx[0] + 1, dtype=np.int32)
-    ys = np.arange(tile_mn[1], tile_mx[1] + 1, dtype=np.int32)
-    poses = np.array(np.meshgrid(xs, ys)).T.reshape(-1, 2)
-    zs = lod * np.ones(len(poses), dtype=np.int32)
-    poses = np.concatenate([poses, zs[:, None]], axis=1)
-    return poses
-
-
-@lru_cache(maxsize=8)
-def _rough_gen(tile_mn, tile_mx, lod, provider, fetcher, parallel, progress):
-    if provider is None:
-        def func(pos):
-            return (pos[0], fetcher(tuple(pos.tolist()), as_array=True))
-    else:
-        def func(pos):
-            return (pos[0], fetcher(tuple(pos.tolist()), provider=provider, as_array=True))
-
-    tile_mn = np.array(tile_mn, np.int32)
-    tile_mx = np.array(tile_mx, np.int32)
-    tile_size = tile_mx - tile_mn
-    poses = _tile_grid(tile_mn, tile_mx, lod)
-    if parallel:
-        pool = ThreadPool()
-        tiles = pool.imap(func, poses)
-        if progress:
-            tiles = list(tqdm(tiles, total=len(poses)))
-        else:
-            tiles = list(tiles)
-        pool.close()
-        pool.join()
-    else:
-        if progress:
-            poses = tqdm(poses)
-        tiles = list(map(func, poses))
-    images = [list() for _ in range(tile_size[0] + 1)]
-
-    for tile in tiles:
-        x, image = tile
-        images[x - poses[0][0]].append(image)
-
-    images = [np.concatenate(row, axis=0) for row in images]
-    image = np.concatenate(images, axis=1)
-    return image
-
-
-def generate_map(geo1, geo2, lod=18, provider=None, progress=False, parallel=True, as_array=False, fetcher=None):
-    if fetcher is None:
-        fetcher = fetch_tile
-    tile_mn, tile_mx, tile_mn_frac, tile_mx_frac = calculate_coverage(geo1, geo2, lod)
-    tile_mn = tuple(map(int, tile_mn))
-    tile_mx = tuple(map(int, tile_mx))
-    image = _rough_gen(tile_mn, tile_mx, lod, provider, fetcher, parallel, progress)
-    tile_mx_frac -= 256
-    e0 = tile_mx_frac[0] if tile_mx_frac[0] != 0 else None
-    e1 = tile_mx_frac[1] if tile_mx_frac[1] != 0 else None
-    image_cropped = image[tile_mn_frac[1]:e1, tile_mn_frac[0]:e0]
-    if not as_array:
-        image_cropped = Image.fromarray(image_cropped)
-    return image_cropped
-
-
 def split_map(image, geo1, geo2, lod=18, as_array=False):
     if isinstance(image, Image.Image):
         image = np.array(image).astype(np.uint8)
@@ -104,7 +127,8 @@ def split_map(image, geo1, geo2, lod=18, as_array=False):
         image_full_size = image_full_size[::-1]
     image_size = 256 * tile_size + tile_mx_frac - tile_mn_frac
     if not np.equal(image_size[::-1], image.shape[:2]).all():
-        image = cv2.resize(image, tuple(image_size), interpolation=cv2.INTER_NEAREST)
+        image = cv2.resize(image, tuple(image_size),
+                           interpolation=cv2.INTER_NEAREST)
     padded = np.zeros(image_full_size, dtype=image.dtype)
     tile_mx_frac -= 256
     e0 = tile_mx_frac[0] if tile_mx_frac[0] != 0 else None
